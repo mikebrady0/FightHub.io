ecf8bb0070a62082557a5971ffde4cb986b608a9
mikebrady0 <147462552+mikebrady0@users.noreply.github.com>
2024-07-27 18:47:05 -0400

    Home folder reworked

--- fighthub/app/home/page.js
+++ fighthub/app/home/page.js
@@ -1,6 +1,6 @@
-import Header from './components/Header';
-import Footer from './components/Footer'; 
-import Intro from './components/Intro';
+import Header from '../components/Header';
+import Footer from '../components/Footer'; 
+import Intro from '../components/Intro';
 
 export default function Home() {
   return (
